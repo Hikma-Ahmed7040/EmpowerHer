967f50a21ec62bbdf7857d9ba4b90f887035c4b9
Hikma Ahmed <hikmahmed497@gmail.com>
2025-03-25 00:11:43 +0300

    pushing page unrespond project

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../Navbar/Navbar.jsx";
 import Hero from "../hero/Hero.jsx";
-// import TraditionalClothesPage from "../traditional_clothes/TraditionalClothes.jsx";
+import TraditionalClothesPage from "../traditional_clothes/TraditionalClothes.jsx";
 import bgImage from "../../assets/adisalem.jpeg"; // Adjust the path based on your project structure
 
 const Home = () => {
