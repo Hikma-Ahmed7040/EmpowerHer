1bd3ae0948c0b37ed87219d6e3e2c62c40f069bf
Hikma Ahmed <hikmahmed497@gmail.com>
2025-04-06 17:05:04 +0300

    dynamic hero page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import Navbar from "./components/Navbar";
 import Banners from "./components/banners/banners"; // Corrected import
 // import TraditionalClothesPage from "./components/TraditionalClothes/traditionalClothes";
 import Home from "./components/Home";
+import ProductDetails from "./pages/productDetails";
 
 function App() {
   return (
     <div>
+      
       <Navbar />
       <Home />
       <Banners /> 
+      <ProductDetails />
       {/* <TraditionalClothesPage/> */}
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
